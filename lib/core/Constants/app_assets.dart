d260ebba1db748b1773d850288cea7c100ed9eac
Muhammad Shehzad <shezikhan2014pk@gmail.com>
2026-03-12 11:37:15 +0500

    now splash screen is complated

--- lib/core/Constants/app_assets.dart
+++ lib/core/Constants/app_assets.dart
@@ -1,4 +1,5 @@
 class AppAssets {
   //Main Logo
-  static const String onBardingLogo = "assets/logo/onBoardingLogo.png";
+  static const String mainLogo = "assets/logo/onBoardingLogo.png";
+  static const String splashScreenBackFrame = "assets/logo/Frame.png";
 }
