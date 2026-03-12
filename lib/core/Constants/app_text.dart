872df99f2ae31ba241800630e54bbe95d6bd5c7d
Muhammad Shehzad <shezikhan2014pk@gmail.com>
2026-03-12 21:42:37 +0500

    in this brach i add all the texts of the give 10 screens

--- lib/core/Constants/app_text.dart
+++ lib/core/Constants/app_text.dart
@@ -1,16 +1,52 @@
 class AppText {
   // App
-  static const String appName = 'Looks Lab';
+  static const String appName = 'SportFinding';
   static const String noRouteFound = 'No Route Found';
 
-  // Auth
-  static const String signIn = 'Sign In';
-  static const String welcome = 'Welcome';
-  static const String yourTransformationBegins =
-      'Your Transformation begins now';
+  // Auth login
+
+  static const String welcome = 'Welcome Back';
+  static const String loginToContinue = 'Login to continue';
+  static const String forgetPassword = 'Forget password?';
+  static const String yourEmail = 'Your Email';
+  static const String password = 'Password';
+
   static const String continueWithGoogle = 'Continue with Google ';
-  static const String continueWithApple = 'Continue with Apple ';
-  static const String subscriptionActivated = 'Subscription activated';
+
+  //Auth sign up
+  static const String createAccount = 'Create Account';
+  static const String joinSportFindingToday = 'Join SportFinding today';
+  static const String fullName = 'Full Name';
+  static const String phoneNumber = 'Phone Number';
+  static const String confirmPassword = 'Confirm Password';
+
+  static const String iAgreeToThe = 'I agree to the';
+  static const String termsOfService = 'Terms of Service';
+  static const String and = 'and';
+  static const String privacyPolicy = 'Privacy Policy';
+
+  //Skill Level
+  static const String whatYourSkillLevel = 'What\'s your skill level?';
+  static const String thisHelpUsFindTheRightTheMatchesForYou =
+      'This helps us find the right matches for you';
+  static const String beginner = 'Beginner';
+  static const String intermedite = 'Intermedite';
+  static const String advance = 'Advance';
+
+  //Choose Sport
+  static const String chooseYourSports = '';
+  static const String selectTheSportsYouEnjoyingPlaying = '';
+  static const String footBall = 'FootBall';
+  static const String basketBall = 'BasketBall';
+  static const String tennis = 'Tennis';
+  static const String vollyBall = 'VollyBall';
+  static const String cricket = 'Ciricket';
+
+  //Location
+  static const String allowLocationAccess = 'Allow Location Access';
+  static const String discoverNearBySportMatchesAndPlayersInYourArea =
+      'Discover nearby sports matches and players in your area.';
+  static const String skipForNow = 'Skip for Now';
 
   // Start
   static const String getStarted = 'Get Started';
@@ -19,13 +55,28 @@ class AppText {
       'Build strong habits every day, Train your body and mind Become your best version';
 
   // OnBoard
-  static const String onBoard = 'OnBoard';
-  static const String next = 'Next';
+  static const String findSportsNearYou = 'Find Sports Near You';
+  static const String discoverPlayerAndSportsMatchesHeppeningInYourArea =
+      'Discover players and sports matches happening in your area.';
+  static const String connectWithPlayers = 'Connect With Players';
+  static const String joinGanesOrInvitePlayersToMatches =
+      'Join games or invite players to matches.';
+  static const String playAndImprove = 'Play and Improve';
+  static const String complatedWithPlayesOfYourSkillLevelAndEnjoeySports =
+      'Compete with players of your skill level and enjoy sports.';
 
   // Home
-  static const String wellnessOverview = 'Wellness Overview';
-  static const String weeklyProgressScore = 'Weekly Progress Score';
+  static const String heyGoodEvening = 'Hey, Good Evening!';
+  static const String findAMatch = 'Find a Match';
+  static const String searchSportOrLocation = 'Search sports or locations...';
+  static const String findPlayers = 'Find Players';
+  static const String upComingMatches = 'Upcoming Matches';
+  static const String viewAll = 'View All';
 
   // Button
-  static const String button1 = 'Button1';
+  static const String allowLocation = 'Allow Location';
+  static const String signUp = 'Sign Up';
+  static const String signIn = 'Sign In';
+  static const String login = 'Login';
+  static const String next = 'next';
 }
