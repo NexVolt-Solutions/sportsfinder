d84c9dd92ff5dda668e887523051b4d6deba8916
Muhammad Shehzad <shezikhan2014pk@gmail.com>
2026-03-12 20:47:17 +0500

    splashScreen background copmlate

--- lib/feature/view/SplashScreen/splash_screen.dart
+++ lib/feature/view/SplashScreen/splash_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:sport_finding/core/Constants/app_assets.dart';
+import 'package:sport_finding/core/Constants/app_colors.dart';
 import 'package:sport_finding/core/Constants/size_extension.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -10,13 +10,85 @@ class SplashScreen extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          Image.asset(
-            AppAssets.splashScreenBackImage,
-            height: context.h(double.infinity),
-            width: context.w(double.infinity),
-            fit: BoxFit.fill,
+          Positioned(
+            top: 0,
+            left: -context.w(55),
+            child: Container(
+              height: context.h(132),
+              width: context.w(144),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(
+                    color: AppColors.pimaryColor.withOpacity(0.2),
+                    offset: const Offset(5, 5),
+                    blurRadius: 30,
+                  ),
+                ],
+              ),
+            ),
           ),
-          Center(child: Image.asset(AppAssets.mainLogo)),
+          Positioned(
+            top: context.h(144),
+            right: -context.w(55),
+            child: Container(
+              height: context.h(132),
+              width: context.w(144),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(
+                    color: AppColors.pimaryColor.withOpacity(0.2),
+                    offset: const Offset(5, 5),
+                    blurRadius: 30,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            top: context.h(394),
+            left: -context.w(55),
+            child: Container(
+              height: context.h(132),
+              width: context.w(144),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(
+                    color: AppColors.pimaryColor.withOpacity(0.2),
+                    offset: const Offset(5, 5),
+                    blurRadius: 30,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: context.h(0),
+            right: -context.w(55),
+            child: Container(
+              height: context.h(132),
+              width: context.w(144),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(
+                    color: AppColors.pimaryColor.withOpacity(0.2),
+                    offset: const Offset(5, 5),
+                    blurRadius: 30,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          // Image.asset(
+          //   AppAssets.splashScreenBackImage,
+          //   height: context.h(double.infinity),
+          //   width: context.w(double.infinity),
+          //   fit: BoxFit.fill,
+          // ),
+          // Center(child: Image.asset(AppAssets.mainLogo)),
         ],
       ),
     );
