b2a44593a0aa251706858fbaf1f34a4a81a92e6a
Muhammad Shehzad <shezikhan2014pk@gmail.com>
2026-03-13 10:52:09 +0500

    here i add the new screens text

--- lib/core/Constants/app_text.dart
+++ lib/core/Constants/app_text.dart
@@ -34,8 +34,9 @@ class AppText {
   static const String advance = 'Advance';
 
   //Choose Sport
-  static const String chooseYourSports = '';
-  static const String selectTheSportsYouEnjoyingPlaying = '';
+  static const String chooseYourSports = 'Choose Your Sports';
+  static const String selectTheSportsYouEnjoyingPlaying =
+      'Select the sports you enjoy playing.';
   static const String footBall = 'FootBall';
   static const String basketBall = 'BasketBall';
   static const String tennis = 'Tennis';
@@ -73,10 +74,76 @@ class AppText {
   static const String upComingMatches = 'Upcoming Matches';
   static const String viewAll = 'View All';
 
+  //BottomBar Text
+  static const String matches = 'Matches';
+  static const String discover = 'Discover';
+  static const String home = 'Home';
+  static const String chat = 'Chat';
+  static const String profile = 'Profile';
+
+  //create Matches
+  static const String createMatch = 'Create Match';
+  static const String setUpNewGameForOthersToJoin =
+      'Set up a new game for others to join.';
+  static const String sportType = 'Sport Type';
+  static const String eegBasketBall = 'ee.g. Basketball';
+  static const String matchTitle = 'Match Title';
+  static const String giveYourMatchName = 'Give your match a name';
+  static const String date = 'Date';
+  static const String dayMonthYear = 'dd/mm/yyyy';
+  static const String time = 'Time';
+  static const String location = 'Location';
+  static const String enterLocation = 'Enter Location';
+  static const String maximumPlayers = 'Maximum Players';
+  static const String eg10 = 'e.g. 10';
+  static const String skillLevel = 'Skill Level';
+  static const String beginnerIntermediateAdvance =
+      'Beginner / Intermediate / Advanced';
+  static const String description = 'Description';
+  static const String discribeYourMatch = 'Describe your match...';
+
+  //Matche Created
+  static const String matchCreated = 'Match Created!';
+  static const String yourMatchIsLiveShareItWithFrinds =
+      'Your match is live. Share it with friends!';
+  static const String invitePeople = 'Invite People';
+  static const String shareMatch = 'Share Match';
+  static const String viewMatch = 'View Match';
+
+  //
+  static const String all = 'All';
+  static const String basketBallGame = 'Basketball Game';
+  static const String centralPark = 'Central Park';
+  static const String overView = 'Overview';
+  static const String invitePlayers = 'Invite Players';
+  static const String abourThisMatch = 'About this match';
+  static const String frindlyBasketBallGamesAt =
+      'Friendly basketball game at Central Park. All intermediate players welcome. Bring your own ball if possible. We usually play for about 2 hours.';
+  static const String participatedPlayers = 'Participated Players';
+
+  //My Match
+  static const String myMatches = 'My Matches';
+  static const String upComing = 'Upcoming';
+  static const String past = 'Past';
+
+  //Profile Screen Text
+  static const String matchesPlayed = 'Matches Played';
+  static const String rating = 'Rating';
+  static const String mySports = 'My Sports';
+  static const String editProfile = 'Edit Profile';
+  static const String share = 'Share';
+
+  //EditProfile screen
+  static const String changePhoto = 'Change Photo';
+  static const String bio = 'Bio';
+
   // Button
   static const String allowLocation = 'Allow Location';
   static const String signUp = 'Sign Up';
   static const String signIn = 'Sign In';
   static const String login = 'Login';
   static const String next = 'next';
+  static const String createMatches = 'Create Match';
+  static const String joinMatches = 'Join Match';
+  static const String saveChanges = 'Save Changes';
 }
