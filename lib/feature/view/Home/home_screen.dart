d655f6aca2ff64d9a10919150dd19cf4d4cfd03f
Muhammad Shehzad <shezikhan2014pk@gmail.com>
2026-03-17 15:07:21 +0500

    Add new icons and implement MatchDetailCard and PlayerCountWidget for enhanced home screen UI

--- lib/feature/view/Home/home_screen.dart
+++ lib/feature/view/Home/home_screen.dart
@@ -1,179 +1,14 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_svg/flutter_svg.dart';
-// import 'package:provider/provider.dart';
-// import 'package:sport_finding/core/Constants/app_assets.dart';
-// import 'package:sport_finding/core/Constants/app_colors.dart';
-// import 'package:sport_finding/core/Constants/app_theme.dart';
-// import 'package:sport_finding/core/Constants/app_text.dart';
-// import 'package:sport_finding/core/Constants/size_extension.dart';
-// import 'package:sport_finding/feature/view_model/home_screen_view_model.dart';
-// import 'package:sport_finding/feature/widget/card_icon_widget.dart';
-// import 'package:sport_finding/feature/widget/card_widget.dart';
-// import 'package:sport_finding/feature/widget/mainframe.dart';
-// import 'package:sport_finding/feature/widget/normal_text.dart';
-// import 'package:sport_finding/feature/widget/search_bar_widget.dart';
-// import 'package:sport_finding/feature/widget/section_header_widget.dart';
-
-// class HomeScreen extends StatefulWidget {
-//   const HomeScreen({super.key});
-
-//   @override
-//   State<HomeScreen> createState() => _HomeScreenState();
-// }
-
-// class _HomeScreenState extends State<HomeScreen> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return ChangeNotifierProvider(
-//       create: (context) => HomeScreenViewModel(),
-//       child: Consumer<HomeScreenViewModel>(
-//         builder: (context, model, child) => Scaffold(
-//           backgroundColor: Colors.white,
-//           body: SafeArea(
-//             child: MainFrame(
-//               child: ListView(
-//                 padding: context.padSym(h: 20),
-//                 children: [
-//                   SizedBox(height: context.h(20)),
-
-//                   // Header with App Name and Notification Icon
-//                   Row(
-//                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                     children: [
-//                       NormalText(
-//                         crossAxisAlignment: CrossAxisAlignment.start,
-//                         titleText: AppText.appName,
-//                         titleStyle: context.appText.text18W600,
-//                         titleColor: context.appColors.onSurface,
-//                       ),
-//                       SvgPicture.asset(AppAssets.notificationIcon),
-//                     ],
-//                   ),
-
-//                   SizedBox(height: context.h(24)),
-
-//                   // User Profile Section
-//                   Row(
-//                     children: [
-//                       Container(
-//                         height: 50,
-//                         width: 50,
-//                         decoration: BoxDecoration(
-//                           shape: BoxShape.circle,
-//                           color: context.appColors.greyDark,
-//                         ),
-//                       ),
-//                       SizedBox(width: context.w(16)),
-//                       Expanded(
-//                         child: NormalText(
-//                           crossAxisAlignment: CrossAxisAlignment.start,
-//                           titleText: AppText.goodEvening,
-//                           titleStyle: context.appText.text18W600,
-//                           titleColor: context.appColors.onSurface,
-//                           subText: 'Shehzad Khan',
-//                           subStyle: context.appText.text16W400,
-//                           subColor: context.appColors.greylight,
-//                           titleAlign: TextAlign.start,
-//                           subAlign: TextAlign.start,
-//                         ),
-//                       ),
-//                     ],
-//                   ),
-
-//                   SizedBox(height: context.h(20)),
-
-//                   // Search Bar
-//                   SearchBarWidget(),
-
-//                   SizedBox(height: context.h(20)),
-
-//                   SizedBox(
-//                     height: context.h(140),
-//                     child: ListView.separated(
-//                       separatorBuilder: (context, index) =>
-//                           SizedBox(width: context.w(12)),
-//                       scrollDirection: Axis.horizontal,
-//                       itemCount: model.matcheData.length,
-//                       itemBuilder: (context, index) {
-//                         final match = model.matcheData[index];
-
-//                         return CardWidget(
-//                           padding: context.padSym(h: 26, v: 20),
-//                           child: Column(
-//                             mainAxisAlignment: MainAxisAlignment.center,
-//                             children: [
-//                               CardIconWidget(imageAsset: match.imagePath),
-//                               SizedBox(height: context.h(8)),
-//                               NormalText(
-//                                 crossAxisAlignment: CrossAxisAlignment.center,
-//                                 titleText: match.title,
-//                                 titleColor: AppColors.blackcolor,
-//                               ),
-//                             ],
-//                           ),
-//                         );
-//                       },
-//                     ),
-//                   ),
-//                   SizedBox(height: context.h(20)),
-
-//                   // Create Match Section Header
-//                   SectionHeaderWidget(
-//                     title: AppText.createMatchTitle,
-//                     actionText: AppText.viewAll,
-//                     icon: AppAssets.nextIcon,
-//                   ),
-
-//                   SizedBox(height: context.h(12)),
-
-//                   // Match Card
-//                   CardWidget(
-//                     padding: context.padSym(h: 16, v: 18),
-//                     child: Column(
-//                       mainAxisAlignment: MainAxisAlignment.start,
-//                       crossAxisAlignment: CrossAxisAlignment.start,
-//                       children: [
-//                         NormalText(
-//                           crossAxisAlignment: CrossAxisAlignment.start,
-//                           titleText: 'Khan Match',
-//                           titleColor: AppColors.blackcolor,
-//                           subText: AppText.basketball,
-//                           titleAlign: TextAlign.start,
-//                           subAlign: TextAlign.start,
-//                         ),
-//                         SizedBox(height: context.h(12)),
-//                         // Add more content here if needed
-//                         Row(
-//                           children: [
-//                             Icon(Icons.location_on_outlined, size: 16),
-//                             SizedBox(width: context.w(4)),
-//                             Text(
-//                               'Location Name',
-//                               style: TextStyle(fontSize: context.sp(14)),
-//                             ),
-//                           ],
-//                         ),
-//                       ],
-//                     ),
-//                   ),
-//                 ],
-//               ),
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 import 'package:sport_finding/core/Constants/app_assets.dart';
 import 'package:sport_finding/core/Constants/app_colors.dart';
+import 'package:sport_finding/core/Constants/app_styles.dart';
 import 'package:sport_finding/core/Constants/app_theme.dart';
 import 'package:sport_finding/core/Constants/app_text.dart';
 import 'package:sport_finding/core/Constants/size_extension.dart';
 import 'package:sport_finding/feature/view_model/home_screen_view_model.dart';
+import 'package:sport_finding/feature/view_model/match_detai_card.dart';
 import 'package:sport_finding/feature/widget/card_icon_widget.dart';
 import 'package:sport_finding/feature/widget/card_widget.dart';
 import 'package:sport_finding/feature/widget/mainframe.dart';
@@ -288,46 +123,65 @@ class _HomeScreenState extends State<HomeScreen> {
                     icon: AppAssets.nextIcon,
                   ),
                   SizedBox(height: context.h(12)),
-                  CardWidget(
-                    padding: context.padSym(h: 16, v: 18),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        NormalText(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          titleText: 'Khan Match',
-                          titleColor: AppColors.blackcolor,
-                          subText: AppText.basketball,
-                        ),
-                        SizedBox(height: context.h(12)),
-                        Row(
-                          children: [
-                            Icon(Icons.location_on_outlined, size: 16),
-                            SizedBox(width: context.w(8)),
-                            NormalText(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              titleText: AppText.locationLabel,
-                              titleStyle: context.appText.text12W600,
-                              titleColor: context.appColors.onSurface,
-                            ),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            SvgPicture.asset(AppAssets.locationIcon),
-                            SizedBox(width: context.w(8)),
-                            NormalText(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              titleText: AppText.locationLabel,
-                              titleStyle: context.appText.text12W600,
-                              titleColor: context.appColors.onSurface,
-                            ),
-                          ],
-                        ),
-                      ],
+                  SizedBox(
+                    height: context.h(200),
+                    width: double
+                        .infinity, // no need for context.w(double.infinity)
+                    child: ListView.separated(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 4, // Show 4 cards
+                      padding: context.padSym(h: 0), // Optional padding
+                      itemBuilder: (context, index) {
+                        return MatchDetilCard(
+                          headingtitle: 'Khan Match',
+                          headingSubtitle: AppText.basketball,
+                          loc: AppText.locationLabel,
+                          time: '16/03/2026, 7:00 PM',
+                          palyerNo: '10/10',
+                        );
+                      },
+                      separatorBuilder: (BuildContext context, int index) {
+                        return SizedBox(
+                          width: context.w(12),
+                        ); // Horizontal spacing between cards
+                      },
                     ),
                   ),
                   SizedBox(height: context.h(20)),
+                  SectionHeaderWidget(title: AppText.popularSports),
+                  SizedBox(height: context.h(16)),
+                  SizedBox(
+                    height: context.h(130),
+                    width: double
+                        .infinity, // no need for context.w(double.infinity)
+                    child: ListView.separated(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 4, // Show 4 cards
+                      padding: context.padSym(h: 0), // Optional padding
+                      itemBuilder: (context, index) {
+                        return CardWidget(
+                          padding: context.padSym(h: 34, v: 18),
+                          child: Column(
+                            children: [
+                              CardIconWidget(
+                                imageAsset: AppAssets.footBallIcon,
+                              ),
+                              NormalText(
+                                titleText: AppText.football,
+                                titleColor: context.appColors.greyDark,
+                                titleStyle: context.appText.text14W600,
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                      separatorBuilder: (BuildContext context, int index) {
+                        return SizedBox(
+                          width: context.w(12),
+                        ); // Horizontal spacing between cards
+                      },
+                    ),
+                  ),
                 ],
               ),
             ),
---
+++ lib/feature/view_model/player_count_widget.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:sport_finding/core/Constants/app_theme.dart';
+import 'package:sport_finding/core/Constants/size_extension.dart';
+import 'package:sport_finding/feature/widget/normal_text.dart';
+
+class PlayerCountWidget extends StatelessWidget {
+  final playerNo;
+  const PlayerCountWidget({super.key, this.playerNo});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(
+          width: 80,
+          height: 25,
+          child: Stack(
+            children: [
+              _avatar(left: 0, context: context),
+              _avatar(left: 20, context: context),
+              _avatar(left: 40, context: context),
+            ],
+          ),
+        ),
+        NormalText(
+          titleText: playerNo,
+          titleColor: context.appColors.greyDark,
+          titleStyle: context.appText.text12W600,
+        ),
+      ],
+    );
+  }
+
+  Widget _avatar({required double left, required BuildContext context}) {
+    return Positioned(
+      left: left,
+      child: Container(
+        padding: context.padAll(4),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: context.appColors.onPrimary,
+          boxShadow: [
+            BoxShadow(
+              color: context.appColors.greyDark,
+              offset: const Offset(0, 4),
+              blurRadius: 60,
+              blurStyle: BlurStyle.inner,
+            ),
+          ],
+        ),
+        child: const Icon(Icons.person, size: 16, color: Colors.black54),
+      ),
+    );
+  }
+}
