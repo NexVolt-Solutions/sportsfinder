52627050358e9d4404e93dc8f56514fd8fa8a17c
Muhammad Shehzad <shezikhan2014pk@gmail.com>
2026-03-31 15:24:56 +0500

    feat: initialize main entry point with MaterialApp and UserScreen home

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:sport_finding/core/Constants/app_theme.dart';
-import 'package:sport_finding/core/Routes/routes.dart';
-import 'package:sport_finding/core/Routes/routes_name.dart';
 import 'package:sport_finding/feature/view/user_screen.dart';
 
 void main() {
