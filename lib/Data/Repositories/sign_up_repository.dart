a7c26cc7eef7833527a1c87306fffbb0338f1a27
Muhammad Shehzad <shezikhan2014pk@gmail.com>
2026-04-10 09:49:54 +0500

    feat: implement authentication flow with sign-up, login, and OTP verification repositories and view models

--- lib/Data/Repositories/sign_up_repository.dart
+++ lib/Data/Repositories/sign_up_repository.dart
@@ -1,4 +1,39 @@
-import 'dart:io';
+// import 'dart:io';
+// import 'package:sport_finding/core/Network/api_service.dart';
+
+// class SignUpRepository {
+//   final ApiService apiService;
+
+//   SignUpRepository({required this.apiService});
+
+//   Future<void> signUpUser({
+//     required String fullName,
+//     required String email,
+//     required String phone,
+//     required String password,
+//     required String confirmPassword,
+//     required bool acceptTerms,
+//     File? image,
+//   }) async {
+//     final response = await apiService.postMultipart(
+//       "/api/v1/auth/register",
+//       fields: {
+//         "full_name": fullName,
+//         "email": email,
+//         "phone": phone,
+//         "password": password,
+//         "confirm_password": confirmPassword,
+//         "accept_terms": acceptTerms.toString(),
+//       },
+//       file: image,
+//       fileField: "profile_image",
+//     );
+
+//     return response;
+//   }
+// }
+
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:sport_finding/core/Network/api_service.dart';
 
 class SignUpRepository {
@@ -9,26 +44,30 @@ class SignUpRepository {
   Future<void> signUpUser({
     required String fullName,
     required String email,
-    required String phone,
+    // required String phone,
     required String password,
     required String confirmPassword,
     required bool acceptTerms,
-    File? image,
+    String? imagePath, // 📱 Mobile
+    List<int>? imageBytes, // 🌐 Web
+    String? imageName, // 🌐 Web
   }) async {
     final response = await apiService.postMultipart(
       "/api/v1/auth/register",
       fields: {
         "full_name": fullName,
         "email": email,
-        "phone": phone,
+        // "phone": phone,
         "password": password,
         "confirm_password": confirmPassword,
         "accept_terms": acceptTerms.toString(),
       },
-      file: image,
+      filePath: kIsWeb ? null : imagePath,
+      fileBytes: kIsWeb ? imageBytes : null,
+      fileName: kIsWeb ? imageName : null,
       fileField: "profile_image",
     );
-
+    print(response.toString());
     return response;
   }
 }
--- lib/feature/view/Auth/SigUp/sign_up.dart
+++ lib/feature/view/Auth/SigUp/sign_up.dart
@@ -1,4 +1,197 @@
+// import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+// import 'package:sport_finding/core/Constants/app_assets.dart';
+// import 'package:sport_finding/core/Constants/app_theme.dart';
+// import 'package:sport_finding/core/Constants/app_text.dart';
+// import 'package:sport_finding/core/Constants/size_extension.dart';
+// import 'package:sport_finding/core/Routes/routes_name.dart';
+// import 'package:sport_finding/feature/view/Auth/SigUp/signup_viewmodel.dart';
+// import 'package:sport_finding/feature/widget/app_bar_widget.dart';
+// import 'package:sport_finding/feature/widget/auth_footer_text.dart';
+// import 'package:sport_finding/feature/widget/custom_button.dart';
+// import 'package:sport_finding/feature/widget/mainframe.dart';
+// import 'package:sport_finding/feature/widget/normal_text.dart';
+// import 'package:sport_finding/feature/widget/profile_avatar_picker.dart';
+// import 'package:sport_finding/feature/widget/social_button_widget.dart';
+// import 'package:sport_finding/feature/widget/terms_check_box.dart';
+// import 'package:sport_finding/feature/widget/text_form_field_widget.dart';
+
+// class SignUp extends StatelessWidget {
+//   const SignUp({super.key});
+
+//   Future<void> _onPickAvatar(BuildContext context) async {
+//     final vm = context.read<SignUpViewModel>();
+//     final message = await vm.pickProfileImageFromGallery();
+//     if (!context.mounted || message == null) return;
+//     ScaffoldMessenger.of(
+//       context,
+//     ).showSnackBar(SnackBar(content: Text(message)));
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Consumer<SignUpViewModel>(
+//       builder: (context, model, _) {
+//         return Scaffold(
+//           body: MainFrame(
+//             child: Form(
+//               key: model.formKey,
+//               child: ListView(
+//                 padding: context.padSym(h: 20),
+//                 children: [
+//                   AppBarWidget(
+//                     onTapFirst: () => Navigator.pop(context),
+//                     title: AppText.sportFinding,
+//                   ),
+//                   NormalText(
+//                     titleText: AppText.createAccount,
+//                     subText: AppText.joinSportFindingToday,
+//                   ),
+//                   SizedBox(height: context.h(16)),
+//                   Center(
+//                     child: Selector<SignUpViewModel, String?>(
+//                       selector: (_, m) => m.profileImagePath,
+//                       builder: (context, imagePath, _) {
+//                         return ProfileAvatarPicker(
+//                           radius: context.radiusR(50),
+//                           imagePath: imagePath,
+//                           onPickPressed: () => _onPickAvatar(context),
+//                         );
+//                       },
+//                     ),
+//                   ),
+//                   SizedBox(height: context.h(12)),
+//                   NormalText(
+//                     crossAxisAlignment: CrossAxisAlignment.center,
+//                     titleText: AppText.uploadYourPicture,
+//                     titleStyle: context.appText.text18W600,
+//                     titleColor: context.appColors.primary,
+//                   ),
+//                   SizedBox(height: context.h(16)),
+//                   TextFormFieldWidget(
+//                     label: AppText.fullName,
+//                     hintText: AppText.fullNameHit,
+//                     controller: model.fullNameController,
+//                     validator: (value) {
+//                       if (value == null || value.isEmpty) {
+//                         return AppText.fullNameValidation;
+//                       }
+//                       return null;
+//                     },
+//                   ),
+//                   SizedBox(height: context.h(16)),
+//                   TextFormFieldWidget(
+//                     label: AppText.email,
+//                     hintText: AppText.email,
+//                     controller: model.emailController,
+//                     validator: (value) {
+//                       if (value == null || value.isEmpty) {
+//                         return AppText.emailValidation;
+//                       }
+//                       return null;
+//                     },
+//                   ),
+//                   SizedBox(height: context.h(16)),
+//                   // TextFormFieldWidget(
+//                   //   label: AppText.phoneNumber,
+//                   //   hintText: AppText.phoneNumberHit,
+//                   //   controller: model.phoneNumberController,
+//                   //   validator: (value) {
+//                   //     if (value == null || value.isEmpty) {
+//                   //       return AppText.phoneNumberValidation;
+//                   //     }
+//                   //     return null;
+//                   //   },
+//                   // ),
+//                   TextFormFieldWidget(
+//                     label: AppText.password,
+//                     hintText: AppText.passwordHit,
+//                     controller: model.passwordController,
+//                     validator: (value) {
+//                       if (value == null || value.isEmpty) {
+//                         return AppText.passwordValidation;
+//                       }
+//                       return null;
+//                     },
+//                   ),
+//                   SizedBox(height: context.h(16)),
+//                   TextFormFieldWidget(
+//                     label: AppText.confirmPassword,
+//                     hintText: AppText.passwordHit,
+//                     controller: model.confirmPasswordController,
+//                     validator: (value) {
+//                       if (value == null || value.isEmpty) {
+//                         return AppText.confirmPasswordValidation;
+//                       }
+//                       return null;
+//                     },
+//                   ),
+//                   SizedBox(height: context.h(8)),
+//                   const TermsCheckbox(),
+//                   SizedBox(height: context.h(8)),
+//                   CustomButton(
+//                     onTap: () async {
+//                       final vm = context.read<SignUpViewModel>();
+
+//                       final error = await vm.registerUser();
+
+//                       if (!context.mounted) return;
+
+//                       if (error == null) {
+//                         ScaffoldMessenger.of(context).showSnackBar(
+//                           const SnackBar(
+//                             content: Text("Registration Successful"),
+//                           ),
+//                         );
+
+//                         Navigator.pushNamed(
+//                           context,
+//                           RoutesName.otpVerificationScreen,
+//                         );
+//                       } else {
+//                         ScaffoldMessenger.of(
+//                           context,
+//                         ).showSnackBar(SnackBar(content: Text(error)));
+//                       }
+//                     },
+//                     text: AppText.signIn,
+//                     color: context.appColors.primary,
+//                   ),
+//                   // CustomButton(
+//                   //   onTap: () => Navigator.pushNamed(
+//                   //     context,
+//                   //     RoutesName.skillLevelScreen,
+//                   //   ),
+//                   //   text: AppText.signIn,
+//                   //   color: context.appColors.primary,
+//                   // ),
+//                   SizedBox(height: context.h(12)),
+//                   SocialButtonWidget(
+//                     imagePath: AppAssets.gmailIcon,
+//                     text: AppText.continueWithGoogle,
+//                     onTap: () {},
+//                   ),
+//                   SizedBox(height: context.h(12)),
+//                   Center(
+//                     child: AuthFooterText(
+//                       normalText: AppText.alreadyHaveAnAccountSignIn,
+//                       actionText: AppText.signIn,
+//                       onTap: () {
+//                         Navigator.pushNamed(context, RoutesName.LoginScreen);
+//                       },
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ),
+//         );
+//       },
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
 import 'package:sport_finding/core/Constants/app_assets.dart';
 import 'package:sport_finding/core/Constants/app_theme.dart';
@@ -20,6 +213,7 @@ class SignUp extends StatelessWidget {
   const SignUp({super.key});
 
   Future<void> _onPickAvatar(BuildContext context) async {
+    final String? imagePath;
     final vm = context.read<SignUpViewModel>();
     final message = await vm.pickProfileImageFromGallery();
     if (!context.mounted || message == null) return;
@@ -49,12 +243,13 @@ class SignUp extends StatelessWidget {
                   ),
                   SizedBox(height: context.h(16)),
                   Center(
-                    child: Selector<SignUpViewModel, String?>(
-                      selector: (_, m) => m.profileImagePath,
-                      builder: (context, imagePath, _) {
+                    // ✅ FIXED: Using XFile? instead of String?
+                    child: Selector<SignUpViewModel, XFile?>(
+                      selector: (_, m) => m.pickedXFile,
+                      builder: (context, xFile, _) {
                         return ProfileAvatarPicker(
                           radius: context.radiusR(50),
-                          imagePath: imagePath,
+                          xFile: xFile, // ✅ pass XFile
                           onPickPressed: () => _onPickAvatar(context),
                         );
                       },
@@ -92,17 +287,6 @@ class SignUp extends StatelessWidget {
                     },
                   ),
                   SizedBox(height: context.h(16)),
-                  // TextFormFieldWidget(
-                  //   label: AppText.phoneNumber,
-                  //   hintText: AppText.phoneNumberHit,
-                  //   controller: model.phoneNumberController,
-                  //   validator: (value) {
-                  //     if (value == null || value.isEmpty) {
-                  //       return AppText.phoneNumberValidation;
-                  //     }
-                  //     return null;
-                  //   },
-                  // ),
                   TextFormFieldWidget(
                     label: AppText.password,
                     hintText: AppText.passwordHit,
@@ -132,21 +316,19 @@ class SignUp extends StatelessWidget {
                   CustomButton(
                     onTap: () async {
                       final vm = context.read<SignUpViewModel>();
-
                       final error = await vm.registerUser();
-
                       if (!context.mounted) return;
-
                       if (error == null) {
                         ScaffoldMessenger.of(context).showSnackBar(
                           const SnackBar(
                             content: Text("Registration Successful"),
                           ),
                         );
-
                         Navigator.pushNamed(
                           context,
                           RoutesName.otpVerificationScreen,
+                          arguments: vm.emailController.text
+                              .trim(), // ✅ sends email
                         );
                       } else {
                         ScaffoldMessenger.of(
@@ -157,14 +339,6 @@ class SignUp extends StatelessWidget {
                     text: AppText.signIn,
                     color: context.appColors.primary,
                   ),
-                  // CustomButton(
-                  //   onTap: () => Navigator.pushNamed(
-                  //     context,
-                  //     RoutesName.skillLevelScreen,
-                  //   ),
-                  //   text: AppText.signIn,
-                  //   color: context.appColors.primary,
-                  // ),
                   SizedBox(height: context.h(12)),
                   SocialButtonWidget(
                     imagePath: AppAssets.gmailIcon,
